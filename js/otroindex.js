3101805e2e2c51ee388ebd4a7e5c2dbc8ff8c578
sofisp <sofisapo927@gmail.com>
2025-05-16 20:59:53 -0300

    Funciones y constantes básicas en JS

--- js/otroindex.js
+++ js/otroindex.js
@@ -22,3 +22,58 @@ let parrafo2 = document.createElement("p");
 parrafo2.innerText ="Párrafo 2";
 parrafo2.style.color ="Green";
 document.body.appendChild(parrafo2);
+
+function suma (a, b) {
+    return a + b;
+}
+
+const resta = function (a, b) {
+    return a - b;
+}
+
+const multiplicacion = (a, b) => {
+    return a * b;
+}
+
+const division = function (a, b) {
+    return a / b;
+}
+
+let a = 3
+let b = 7
+opcion = 1
+
+switch (opcion) {
+    case 1:
+        console.log("El resultado de la suma de " + a + " y " + b + " es " + suma(a, b))
+        break;
+    case 2:
+        console.log("El resultado de la resta entre " + a + " y " + b + " es " + resta(a, b))
+        break;
+    case 3:
+        console.log("El resultado de la multiplicación entre " + a + " y " + b + " es " + multiplicacion(a, b))
+        break;
+    case 4:
+        console.log("El resultado de la división de " + a + " y " + b + " es " + division(a, b))
+        break;
+    default:
+        console.log("Opción inválida")
+}
+
+let opcion2 = 2
+switch (opcion2) {
+    case 1:
+        console.log("El resultado de la suma de " + a + " y " + b + " es " + suma(a, b))
+        break;
+    case 2:
+        console.log("El resultado de la resta entre " + a + " y " + b + " es " + resta(a, b))
+        break;
+    case 3:
+        console.log("El resultado de la multiplicación entre " + a + " y " + b + " es " + multiplicacion(a, b))
+        break;
+    case 4:
+        console.log("El resultado de la división de " + a + " y " + b + " es " + division(a, b))
+        break;
+    default:
+        console.log("Opción inválida")
+}
